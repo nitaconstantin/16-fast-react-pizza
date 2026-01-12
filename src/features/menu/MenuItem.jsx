a6242e51619d55a921caec7a529cd6a8900aa6de
nitaconstantin <104198432+nitaconstantin@users.noreply.github.com>
2026-01-12 23:20:48 +0200

    316 - Adding Menu Items to the Cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,18 +1,48 @@
 import { formatCurrency } from "../../utils/helpers";
-import Button from '../../ui/Button'
+import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+  function handleAddToCart() {
+    // console.log(id);
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
-      <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? 'opacity-70 grayscale': ''}`}/>
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+      />
       <div className="flex  flex-col justify-center w-full pt-0.5">
         <p className="font-medium">{name}</p>
-        <p className="text-sm text-stone-500 italic capitalize">{ingredients.join(", ")}</p>
+        <p className="text-sm text-stone-500 italic capitalize">
+          {ingredients.join(", ")}
+        </p>
         <div className="mt-auto flex items-center justify-between">
-          {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="uppercase text-sm font-medium text-stone-500">Sold out</p>}
-          <Button type='small'>Add to cart</Button>
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="uppercase text-sm font-medium text-stone-500">
+              Sold out
+            </p>
+          )}
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
